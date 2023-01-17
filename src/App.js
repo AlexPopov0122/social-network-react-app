2f09a999524054aeef25e2eaa96e5169e37348c8
Alexander <sahapopov2004@mail.ru>
2023-01-17 08:34:47 +0500

    addiеtion of components

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Profile from "./components/Profile/Profile";
 import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
-  return (
-    <div className = "app-wrapper">
-      <Header/>
-      <NavBar/>
-      <Profile/>
-      <NavBarRight/>
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <Header/>
+            <NavBar/>
+            <Profile/>
+            <NavBarRight/>
+        </div>
+    );
 };
-
 export default App;
